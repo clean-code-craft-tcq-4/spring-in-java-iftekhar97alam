6aaa625d159ab070d0c4b2182b6eb42d3047bcd7
iftekhar97alam <105351481+iftekhar97alam@users.noreply.github.com>
2022-05-22 12:03:40 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,10 +1,72 @@
 package statisticker;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+	public static class Stats{
+		float min = Float.NaN;
+		float max = Float.NaN;;
+		float average = Float.NaN;;
+		
+		Stats(List<Float> numbers){
+			if(!numbers.isEmpty())
+			{
+				this.min = Collections.min(numbers);
+				this.max = Collections.max(numbers);
+				this.average = getAverage(numbers);
+			}
+		}
+		
+		private Float getAverage(List<Float> numbers){
+			Float sum=0.0f;
+			for (Float number : numbers) {
+		        sum += number;
+		    }
+			return sum/numbers.size();
+		}
+	}
+    public static Stats getStatistics(List<Float> numbers) {
+    	return new Stats(numbers);
     }
+}
+
+interface IAlerter{
+	public void setAlert(boolean bool);
+}
+
+class EmailAlert implements IAlerter{
+	boolean emailSent = false;
+	@Override
+	public void setAlert(boolean bool) {
+		this.emailSent = bool;
+	}
+}
+
+class LEDAlert implements IAlerter{
+	boolean ledGlows = false;
+	@Override
+	public void setAlert(boolean bool) {
+		this.ledGlows = bool;
+	}
+}
+
+class StatsChecker{
+	private float max;
+	private IAlerter[] alerters;
+	StatsChecker(float max, IAlerter[] alerters){
+		this.max = max;
+		this.alerters = alerters;
+	}
+	
+	public void checkAndAlert(List<Float> numbers){
+		for (Float number : numbers) {
+	        if(number > max){
+	        	for(IAlerter alerter: alerters)
+	        		alerter.setAlert(true);
+	        	return;
+	        }
+	    }
+	}
 }
